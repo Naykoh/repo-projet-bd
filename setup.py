369f6e7b28189ecadf655a7b00bfabdaa3bf8d32
Naykoh <nayel.hamani@gmail.com>
2020-10-21 19:09:25 +0200

    push all files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The goal of this project is to apply some concepts & tools seen in the 3 parts of this course, this project is organized into 3 parts :',
+    author='nayel ',
+    license='MIT',
+)
